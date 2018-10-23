6ce0b6fb043b1e18dfbbfac0b852f617b3fb4275
ntucci <nicholas.tucci@bigtelematics.com>
2018-10-23 18:20:32 +0100

    grouping by window

--- project/streaming/event_time.py
+++ project/streaming/event_time.py
@@ -1,3 +1,7 @@
+from time import sleep
+
+import pyspark.sql.functions as f
+
 
 class StreamingEventTime:
     def __init__(self, data_path, data_schema, spark):
@@ -11,4 +15,11 @@ class StreamingEventTime:
 
         withEventTime = streaming.selectExpr("*", "cast(cast(Creation_Time as double)/1000000000 as timestamp) as event_time")
 
-        withEventTime.printSchema()
+        window_time = withEventTime.groupBy(f.window(f.col("event_time"), "10 minutes")).count()\
+            .writeStream.queryName("events_per_window").format("memory").outputMode("complete").start()
+
+        for x in range(5):
+            self.spark.sql("SELECT * FROM events_per_window ORDER BY window").show(truncate=False)
+            sleep(1)
+
+        window_time.awaitTermination()
